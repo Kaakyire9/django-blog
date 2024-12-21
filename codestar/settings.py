bb709c0f42fcbf9dac28e2c818f55ab0f048d914
(Sampson) Reginald <r.aduboffour@gmail.com>
2024-12-21 16:20:42 +0000

    Rename index view to my_blog and update URL routing; configure allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jn-bc87c!gogc2rjoh!rh!lzouf)k6%()#&-h68rg^g$tt-*mx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
