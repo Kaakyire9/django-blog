07cfab0fd2de300d4bab11dcf85cef73ab2e285d
(Sampson) Reginald <r.aduboffour@gmail.com>
2024-12-23 09:49:54 +0000

    Set DEBUG to False for production environment

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['800-nielmc-django-project-lxqprmm3qz.us2.codeanyapp.com', '.herokuapp.com', 'localhost']
 
