193615434b536e6cb5ec49d41e12874413ecd71c
(Sampson) Reginald <r.aduboffour@gmail.com>
2025-01-05 14:07:01 +0000

    Refactor About and Blog models, forms, and templates for improved clarity and functionality; update comment handling in JavaScript and enhance responsiveness in templates.

--- about/views.py
+++ about/views.py
@@ -5,7 +5,18 @@ from .forms import CollaborateForm
 
 
 def about_me(request):
-
+    """
+    Renders the most recent information on the website author
+    and allows user collaboration request
+    Display and individual instance of :model:`about.About`.
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About.
+    ``collaboration_form``
+        An instance of :form:`about.CollaborationForm`.
+    **Template:**
+    :template:`about/about.html`
+    """
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
